ef8350381ab6f5b44ab817f298e67e15212f2960
Hassan <hassan.mallah@cleverbots.ru>
2020-11-20 14:51:06 +0300

    project structure

--- app.py
+++ app.py
@@ -24,7 +24,9 @@
 
 from flask_app import create_app
 
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
+
     # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000)
