2a9e24852b8a8d5c3f4e67c65e331f2efc13a4d5
Hassan <hassan.mallah@cleverbots.ru>
2020-11-20 11:10:32 +0300

    adding flask_sqlalchemy

--- app.py
+++ app.py
@@ -3,11 +3,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/others"  # local
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://postgres:postgres@postgres:5432"  # server
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
 
 # A welcome message to test our server
 @app.route('/')
 def index():
-    return "<h1>Welcome to our server !!</h1>"
+    x = db.session.execute('select 1 as is_alive;').fetchall()
+    return "<h1>Welcome to our server !! {}</h1>".format(x)
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
