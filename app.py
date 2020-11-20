c084d3c54d90a40e7d28add18237d632f84a7b0b
Hassan <hassan.mallah@cleverbots.ru>
2020-11-20 11:48:43 +0300

    adding flask_sqlalchemy

--- app.py
+++ app.py
@@ -1,19 +1,11 @@
 # app.py
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
-
 import os
-import psycopg2
-
-DATABASE_URL = os.environ['DATABASE_URL']
-
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/others"  # local
-# app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://postgres:postgres@postgres:5432"  # server
-# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///still-harbor-31837"  # server
+DATABASE_URL = os.environ['DATABASE_URL'] # server
+# DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/others"  # local
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
