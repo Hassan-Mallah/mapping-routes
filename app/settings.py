237930e29598ed287de135316729e1520fa210c5
Hassan <hassan.mallah@cleverbots.ru>
2020-11-20 14:08:06 +0300

    project structure

--- app/settings.py
+++ app/settings.py
@@ -1,4 +1,6 @@
-# DATABASE_URL = os.environ['DATABASE_URL'] # server
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/others"  # local
+import os
+
+DATABASE_URL = os.environ['DATABASE_URL'] # server
+# DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/others"  # local
 SQLALCHEMY_DATABASE_URI = DATABASE_URL
 SQLALCHEMY_TRACK_MODIFICATIONS = True
