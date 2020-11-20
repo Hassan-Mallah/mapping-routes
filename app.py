c4e9c5f129843f77fcca4e4d7daef8dc5aaa06ad
Hassan <hassan.mallah@cleverbots.ru>
2020-11-20 14:48:41 +0300

    project structure

--- app.py
+++ app.py
@@ -1,22 +1,30 @@
-# app.py
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-import os
+# # app.py
+# from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+# import os
+#
+# app = Flask(__name__)
+# DATABASE_URL = os.environ['DATABASE_URL'] # server
+# # DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/others"  # local
+# app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+# db = SQLAlchemy(app)
+#
+#
+# # A welcome message to test our server
+# @app.route('/')
+# def index():
+#     x = db.session.execute('select 1 as is_alive;').fetchall()
+#     return "<h1>Welcome to our server !! {}</h1>".format(x)
+#
+# if __name__ == '__main__':
+#     # Threaded option to enable multiple instances for multiple user access support
+#     app.run(threaded=True, port=5000)
 
-app = Flask(__name__)
-DATABASE_URL = os.environ['DATABASE_URL'] # server
-# DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/others"  # local
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-db = SQLAlchemy(app)
 
-
-# A welcome message to test our server
-@app.route('/')
-def index():
-    x = db.session.execute('select 1 as is_alive;').fetchall()
-    return "<h1>Welcome to our server !! {}</h1>".format(x)
+from flask_app import create_app
 
 if __name__ == '__main__':
+    app = create_app()
     # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000)
