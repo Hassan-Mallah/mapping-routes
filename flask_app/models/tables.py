410abcc9f9fe144b7728b4c32146db2b751f1630
Hassan <hassan.mallah@cleverbots.ru>
2020-11-23 16:46:14 +0300

    tables

--- flask_app/models/tables.py
+++ flask_app/models/tables.py
@@ -4,7 +4,7 @@ from flask_user import UserMixin  # requires email_validator
 
 # Define the User data model. Make sure to add the flask_user.UserMixin !!
 class User(db.Model, UserMixin):
-    __tablename__ = 'users'
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
 
     # User authentication information (required for Flask-User)
@@ -12,3 +12,21 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(255), nullable=False, server_default='')
     # reset_password_token = db.Column(db.String(100), nullable=False, server_default='')
     active = db.Column(db.Boolean(), nullable=False, server_default='0')
+
+
+class Route(db.Model):
+    __tablename__ = 'route'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+
+class Point(db.Model):
+    __tablename__ = 'point'
+    id = db.Column(db.Integer, primary_key=True)
+    route_id = db.Column(db.Integer, db.ForeignKey('route.id'), nullable=False)
+    coordinates_x = db.Column(db.Integer)
+    coordinates_y = db.Column(db.Integer)
+
+
+
--- flask_app/views/main_views.py
+++ flask_app/views/main_views.py
@@ -5,8 +5,8 @@ from flask_user import current_user, login_required, roles_required
 main_blueprint = Blueprint('main', __name__, url_prefix='')
 
 
+# @login_required
 @main_blueprint.route('/')
-@login_required
 def index():
 
     x = db.session.execute('select 1 as is_alive;').fetchall()
