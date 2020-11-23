5882648594e33b38842e7969863d2c91cc246eab
Hassan <hassan.mallah@cleverbots.ru>
2020-11-23 15:33:32 +0300

    flask_user + login

--- flask_app/views/main_views.py
+++ flask_app/views/main_views.py
@@ -1,11 +1,10 @@
 from flask_app import db
 from flask import Blueprint, redirect, render_template
-# from flask_app.models.tables import User
 from flask_user import current_user, login_required, roles_required
 
 main_blueprint = Blueprint('main', __name__, url_prefix='')
 
-print('hey')
+
 @main_blueprint.route('/')
 @login_required
 def index():
